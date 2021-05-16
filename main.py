b54e3b44b75e0485fac148715a14afb7a3d5e4bb
Chintamani Raju Virulkar <54358660+DawoodBhai420@users.noreply.github.com>
2021-05-16 12:43:28 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,47 @@
+import speech_recognition as sr
+import pyttsx3
+import pywhatkit
+import datetime
+
+listener = sr.Recognizer()
+engine = pyttsx3.init()
+voices = engine.getProperty('voices')
+engine.setProperty('voice',voices[1].id)
+def talk(text):
+    engine.say(text)
+    engine.runAndWait()
+greeting = 'Heyyyyyy guys. its Alina. What can I do for you'
+talk(greeting)
+def take_command():
+    try:
+        with sr.Microphone() as source:
+            print('I am listening.....')
+            voice = listener.listen(source)
+            command = listener.recognize_google(voice)
+            command = command.lower()
+            if 'alina' in command:
+                command = command.replace('alina',' ')
+                print(command)
+    except:
+        pass
+    return command
+
+
+def run_alina():
+    command = take_command()
+    if 'play' in command:
+        song = command.replace('play',' ')
+        print('playing' + song)
+        talk('playing' + song)
+        pywhatkit.playonyt(song)
+    elif 'time' in command:
+        time = datetime.datetime.now().strftime('%I %M %p')
+        print(time)
+        talk('current time is '+time)
+
+
+
+while True:
+    run_alina()
+
+

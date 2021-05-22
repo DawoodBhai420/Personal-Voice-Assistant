ad110797a1facc05ec1ed8c8e53f6b2fc144c971
Chintamani Raju Virulkar <chintamani.virulkar@gmail.com>
2021-05-22 21:35:28 +0530

    added date and updated website opener

--- main.py
+++ main.py
@@ -5,17 +5,23 @@ import datetime
 import wikipedia
 import webbrowser as wb
 
+wb.register('chrome', None)
+wb.BackgroundBrowser("C:\Program Files\Google\Chrome\Application\chrome.exe")
+
 listener = sr.Recognizer()
 engine = pyttsx3.init()
 voices = engine.getProperty('voices')
-engine.setProperty('voice',voices[1].id)
+engine.setProperty('voice', voices[1].id)
+
 
 def talk(text):
     engine.say(text)
     engine.runAndWait()
 
+
 greeting = 'Heyyyyyy guys. its Alina. What can I do for you'
 talk(greeting)
+
 
 def take_command():
     try:
@@ -25,7 +31,7 @@ def take_command():
             command = listener.recognize_google(voice)
             command = command.lower()
             if 'alina' in command:
-                command = command.replace('alina',' ')
+                command = command.replace('alina', ' ')
                 print(command)
     except:
         pass
@@ -35,27 +41,30 @@ def take_command():
 def run_alina():
     command = take_command()
     if 'play' in command:
-        song = command.replace('play',' ')
+        song = command.replace('play', ' ')
         print('playing' + song)
         talk('playing' + song)
         pywhatkit.playonyt(song)
     elif 'time' in command:
         time = datetime.datetime.now().strftime('%I %M %p')
         print(time)
-        talk('current time is '+time)
+        talk('current time is ' + time)
+    elif 'date' in command:
+        date = datetime.datetime.now()
+        final = [date.day, date.strftime('%B'), date.year,date.strftime('%A')]
+        print(final)
+        talk(final)
     elif 'tell me about' in command:
-        thing = command.replace('tell me about',' ')
-        info = wikipedia.summary(thing,1)
+        thing = command.replace('tell me about', ' ')
+        info = wikipedia.summary(thing, 1)
         print(info)
         talk(info)
-    elif '.com' in command:
-        site = command.replace('open',' ')
+    elif '.com' or '.in' or '.org' or '.edu.in' in command:
+        site = command.replace('open', ' ')
         print('opening' + site)
         talk('opening' + site)
-        wb.open(site)
+        wb.get('chrome').open(site)
 
 
 while True:
     run_alina()
-
-
